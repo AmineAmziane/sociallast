932e0c6b7b48d9e2e60b38d06724f264833f0931
AmineDz <amine2014max@gmail.com>
2020-09-15 04:27:23 +0100

    Updates

---
+++ precache-manifest.1b631ec8716d93e6a85e2d1123af2f03.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ff891d0c983d6354203e054eea50bb",
+    "url": "/socialgit/index.html"
+  },
+  {
+    "revision": "4670519df6d1c4da6b91",
+    "url": "/socialgit/static/css/main.dfa0971a.chunk.css"
+  },
+  {
+    "revision": "2e7affecbc20afd04531",
+    "url": "/socialgit/static/js/2.d4040aea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/socialgit/static/js/2.d4040aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4670519df6d1c4da6b91",
+    "url": "/socialgit/static/js/main.74d1386b.chunk.js"
+  },
+  {
+    "revision": "164181fcc93fc7b41035",
+    "url": "/socialgit/static/js/runtime-main.fa1589ae.js"
+  },
+  {
+    "revision": "1ccf745a6ea3ba0e58ee2a962ece2fbd",
+    "url": "/socialgit/static/media/bg-pattern-bottom-desktop.1ccf745a.svg"
+  },
+  {
+    "revision": "02b87d6740fcff00e2d794d4db5f4f8d",
+    "url": "/socialgit/static/media/bg-pattern-bottom-mobile.02b87d67.svg"
+  },
+  {
+    "revision": "0e1f3813427daeaeebd6bc4f0c89e2a4",
+    "url": "/socialgit/static/media/bg-pattern-top-desktop.0e1f3813.svg"
+  },
+  {
+    "revision": "f55aa9cca88e83b31f78ae4ff7033063",
+    "url": "/socialgit/static/media/bg-pattern-top-mobile.f55aa9cc.svg"
+  }
+]);
